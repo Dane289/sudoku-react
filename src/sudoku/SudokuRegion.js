a2c57118c894410defdeb7327ef7abec85cc1c11
danielst <stan.mariusdan@gmail.com>
2018-07-23 16:03:28 +0300

    turn class into func

--- src/sudoku/SudokuRegion.js
+++ src/sudoku/SudokuRegion.js
@@ -1,37 +1,31 @@
-import React, { Component } from 'react';
 import SudokuInput from './SudokuInput';
 import './Sudoku.css';
+import React from 'react';
 
-class SudokuRegion extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-
-    createRow = (props) => {
-        let row1 = [];
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                row1.push(
-                    <SudokuInput 
-                        key={3 * i + j}
-                        board={props.sudokuBoard} 
-                        locationX={props.regionNo[0] * 3 + i} 
-                        locationY={props.regionNo[1] * 3 + j} 
-                        changeHandler={props.handleChange}
-                    />);
-            }
+const createRow = (props) => {
+    let row1 = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            row1.push(
+                <SudokuInput
+                    key={3 * i + j}
+                    board={props.sudokuBoard}
+                    locationX={props.regionNo[0] * 3 + i}
+                    locationY={props.regionNo[1] * 3 + j}
+                    changeHandler={props.handleChange}
+                />);
         }
-        return row1;
     }
-
-    render() {
-        return (
-            <div className="region "> 
-                {this.createRow(this.props)}
-            </div>
-        );
-    }
+    return row1;
 }
+
+const SudokuRegion = (props) => {
+    return (
+        <div className="region ">
+            {createRow(props)}
+        </div>
+    );
+}
+
 
 export default SudokuRegion
