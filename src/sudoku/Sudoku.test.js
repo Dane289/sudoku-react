9e8fefa8eb6cac8f9a7f84d03b821a2106673538
danielst <stan.mariusdan@gmail.com>
2018-08-06 09:49:11 +0300

    Fix failling test

--- src/sudoku/Sudoku.test.js
+++ src/sudoku/Sudoku.test.js
@@ -6,6 +6,6 @@ import SudokuInput from './SudokuInput';
 
 it('renders without crashing', () => {
     let div = document.createElement("div");
-    ReactDOM.render(<Sudoku/>, div);
+    ReactDOM.render(<Sudoku sudokuBoard={Array.apply(null, Array(82)).map(Number.prototype.valueOf,0)} />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
